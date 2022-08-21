e3d739dbe1ebc5769d084d1419d1d5356abc6c06
Pavel <99116523+TestingInfo@users.noreply.github.com>
2022-08-21 16:45:17 +0300

    Add files via upload
    Задания по курсу

---
+++ HW 1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+/* Реализуйте алгоритм такой задачи
+
+Программа получает на вход имя, возраст и размер запрашиваемого кредита
+
+Кредит выдается если:
+
+Имя не "Bob"
+
+Возраст 18 лет и больше
+
+Сумма запрашиваемого кредита не больше возраста умноженного на 100 (для 25 летнего предельная сумма кредита = 2500)
+
+В других случаях кредит не выдается Выдача кредита - это вывод в консоль сообщения, что кредит выдан.
+ */
+class Program
+{
+    public static void printNoCredit()
+    {
+        Console.WriteLine("Кредит не может быть выдан");
+    }
+
+    public static string ClientName()
+    {
+        Console.Write("Введите ваше имя:");
+        Console.WriteLine();
+        string clientName = Console.ReadLine();
+        bool check = false;
+        for (int i = 0; i < clientName.Length; i++)
+        {
+            if (clientName[i] >= 'a' && clientName[i] <= 'z' || clientName[i] == ' ')
+            {
+                check = true;
+            }
+
+            else
+            {           
+                check = false;
+            }
+        }
+
+        if (check == false)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Вводить можно только латинские буквы" );
+            Console.WriteLine();
+            return ClientName();
+        }
+        return clientName;
+    }
+    public static int ClientAge()
+    {
+        Console.WriteLine("Введите ваш возраст: ");
+        try
+        {
+            int clientAge = int.Parse(Console.ReadLine());
+            if (clientAge>=18 && clientAge < int.MaxValue) {
+                return clientAge;
+            }
+            
+            else {
+                printNoCredit();
+                return ClientAge();
+            }
+        }
+        catch {
+            Console.WriteLine("Введите ваш возраст. Пример: 50");
+            return ClientAge();
+        }
+    }
+    public static int SumCredit()
+    {
+        Console.WriteLine("Введите сумму кредита: ");
+
+        try
+        {
+            int sumCredit = int.Parse(Console.ReadLine());
+            if (sumCredit > 0 && sumCredit < int.MaxValue)
+            { }
+            return sumCredit;
+        }
+
+
+        catch {
+            Console.WriteLine("Превышена максимальная сумма кредита");
+            printNoCredit();
+            return SumCredit();
+        }   
+    }
+
+    static void Credit(string clientName, int clientAge, int sumCredit)
+    {
+        if (clientName != "Bob" && clientAge >= 18)
+        {
+            int maxSumCredit = clientAge * 100;
+            if (sumCredit <= maxSumCredit)
+            {
+                Console.WriteLine("Кредит выдан");
+            }
+            else
+            {
+                printNoCredit();
+            }
+        }
+        else
+        {
+            printNoCredit();
+        }
+        Console.Read();
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        Credit(ClientName(), ClientAge(), SumCredit());
+    }
+}
---
+++ HW3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    /* Напишите консольный калькулятор
+Получает на вход выражение вида "1*2" или "50-1" или "45/2"
+Допустимые операции: *, /, +, -
+На вход допускается только выражение с одной операцией
+На вход попадают только целые числа, но результат может быть дробным
+В строке на вход могут быть лишние пробелы вроде "55 -8"
+Вывод приложения = или сообщение об ошибке или результат выражения
+Приложение выдает ошибку как минимум, если входные данные в неверном формате */
+
+
+    public static void Main(string[] args)
+    {
+        try
+        {
+            string userMathOperation = Console.ReadLine();
+
+            char[] operat = { '-', '+', '*', '/' };
+        
+            var arr = userMathOperation.Split(operat, StringSplitOptions.None);
+            
+            int arg1 = int.Parse(arr[0]);
+            int arg2 = int.Parse(arr[1]);
+        
+            if (userMathOperation.Contains("-"))
+                Console.WriteLine((arg1 - arg2));
+            else if (userMathOperation.Contains("+"))
+                Console.WriteLine(arg1 + arg2);
+            else if (userMathOperation.Contains("/"))
+                if (arg2 == 0)
+                {
+                    Console.WriteLine("На ноль делить нельзя");
+                }
+                else
+                {
+                    Console.WriteLine(arg1 * 1.0 / arg2);
+                }
+            else
+                Console.WriteLine(arg1 * arg2);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Вы ввели данные в неверном формате");
+            
+        }
+    }
+}
+
+
+
+
